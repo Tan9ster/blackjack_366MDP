1cde9c0d9ec0018acd6133cd5cfa367d307058c0
Dan <djphan@ualberta.ca>
2014-11-03 19:05:56 -0700

    Update qLearning.py

--- qLearning.py
+++ qLearning.py
@@ -19,42 +19,7 @@ def showOneGame():
     return None
 
 def bjqLearning(Q = numpy.random.rand(182,2)*0.00001, alpha=0.008, gamma = 1, numEpisodes=10000000, epsilon=0):
-    #Initalize the states q(s,a) with small random numbers
-    returnSum = 0.0
 
-    for episodeNum in range(numEpisodes):
-        # Initalize states and G
-        G = 0
-        s=blackjack.init()
 
-            
-        while s!=-1: #-1 is terminal
-            ## Use a uniform random distribution for e greedy selection
-            check = numpy.random.uniform(0, 1)
-           
-            if check < epsilon:
-                # Our action is less than epsilon we explore
-                # Rand int returns a number between 0, 1
-                a=random.randint(0,1)
-
-            else:
-                a=scipy.argmax(Q[s, :]) 
-
-            # Take action a, and observe reward G and state sp
-            G, sp = blackjack.sample(s,a)
-
-            Q[s,a] = Q[s,a] + ( alpha * ( G + ( gamma*max(Q[sp, : ]) ) - Q[s,a] ) )     
-            s = sp
-
-            returnSum = returnSum + G
-
-        if (episodeNum % 10000 == 0 and episodeNum > 0):
-            print("Episode: ", episodeNum, "Average Return: ", returnSum/episodeNum)
-
-    print("Final Average Return: ", returnSum/numEpisodes)
-    blackjack.printPolicy( policy = lambda x: scipy.argmax(Q[x, :]) );
-    return None
-
-bjqLearning()
 
 
